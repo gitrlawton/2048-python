0a498f4d7d57726e4a494cd92007ba02715a81dd
Neal L <rlawton.cs@gmail.com>
2024-05-20 23:04:51 -0700

    Wrote the Tile class, as well as the function to generate two randomly positioned tiles at the start of the game.

--- main.py
+++ main.py
@@ -27,6 +27,57 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
+class Tile:
+    COLORS = [
+        (237, 229, 218),
+        (238, 225, 201),
+        (243, 178, 122),
+        (246, 150, 101),
+        (247, 124, 95),
+        (247, 95, 59),
+        (237, 208, 115),
+        (237, 204, 99),
+        (236, 202, 80),
+    ]
+    
+    def __init__(self, value, row, col):
+        self.value = value         # 2, 4, 8, etc.
+        self.row = row             # row number of the grid.
+        self.col = col             # column number of the grid.
+        self.x = col * RECT_WIDTH  # starting x-coordinate for drawing the tile.
+        self.y = row * RECT_HEIGHT # starting y-coordinate for drawing the tile.
+        
+    # Getter   
+    def get_color(self):
+        # Get the index of the color the tile currently is.
+        color_index = int(math.log2(self.value)) - 1
+        # Retrieve the color from the list of colors using the index.
+        color = self.COLORS[color_index]
+        
+        return color
+    
+    # Method draws the tile on the screen.
+    def draw(self, window):
+        # Draw the tile.
+        color = self.get_color()
+        pygame.draw.rect(window, color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
+        
+        # Set the text to be drawn on top of the tile.
+        text = FONT.render(str(self.value), 1, FONT_COLOR)
+        # Set the position to draw the text at.
+        x_position = self.x + (RECT_WIDTH / 2 - text.get_width() / 2)
+        y_position = self.y + (RECT_HEIGHT / 2 - text.get_height() / 2)
+        # Draw the number at that position.
+        window.blit(text, (x_position, y_position))
+    
+    # Method sets the position of the tile in the grid.
+    def set_position(self):
+        pass
+    
+    # Method moves the tile.
+    def move(self, delta):
+        pass
+
 def draw_grid(window):
     for row in range(1, ROWS):
         y = row * RECT_HEIGHT
@@ -39,17 +90,57 @@ def draw_grid(window):
     # Draw window border.
     pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
 
-def draw(window):
+def draw(window, tiles):
     # Paint the window the background color.
     window.fill(BACKGROUND_COLOR)
     
-    draw_grid(window)
+    # Draw each of the tiles.
+    for tile in tiles.values():
+        tile.draw(window)
     
+    # Draw the grid.
+    draw_grid(window)
+    # Update window.
     pygame.display.update()
+    
+def get_random_pos(tiles):
+    row = None
+    col = None
+    
+    # Until we find a random row-col position that does not yet have a tile 
+    # in it...
+    while True:
+        row = random.randrange(0, ROWS)
+        col = random.randrange(0, COLS)
+        
+        if f"{row}{col}" not in tiles:
+            break
+        
+    return (row, col)
+        
+# Function to generate two initial tiles to start our game.   
+def generate_tiles():
+    # Create empty dictionary for tiles.
+    tiles = {}
+    # Execute loop 2 times.
+    for _ in range(2):
+        # generate random row and column for tile to be placed at.
+        row, col = get_random_pos(tiles)
+        # Add tile to the dictionary using the string version of its row/col 
+        # for the key and the Tile object as the value.
+        tiles[f"{row}{col}"] = Tile(2, row, col)
+        
+    return tiles
 
 def main(window):
     clock = pygame.time.Clock()
     run = True
+    
+    # Tiles is a dictionary of tiles.  The keys are two-digit strings 
+    # representing the row and column. Example: "00" is row 0 col 0 and 
+    # "20" is row 2 col 0.  generate_tiles() generates two random tiles to
+    # start our game.
+    tiles = generate_tiles()
     
     while run:
         clock.tick(FPS)
@@ -58,8 +149,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
-        
-        draw(window)
+            
+        # Draw the tiles on the window.
+        draw(window, tiles)
     
     pygame.quit()
 
